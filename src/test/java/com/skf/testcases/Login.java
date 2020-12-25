bb18d65b58f5a6a85c60eee3667afcb908d6171f
skf-project <74064182+skf-project@users.noreply.github.com>
2020-12-25 18:39:55 +0530

    modification

--- src/test/java/com/skf/testcases/Login.java
+++ src/test/java/com/skf/testcases/Login.java
@@ -1,8 +1,5 @@
 package com.skf.testcases;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -12,7 +9,6 @@ import org.testng.annotations.Test;
 
 import com.skf.base.Page;
 import com.skf.pages.LoginPage;
-import com.skf.pages.TurbinePage;
 
 public class Login extends Page {
 
@@ -27,76 +23,7 @@ public class Login extends Page {
 		LoginPage loginPage = new LoginPage();
 		loginPage.loginApp(config.getProperty("validUsername"),
 				config.getProperty("validPassword"));
-		TurbinePage turbinePage= new TurbinePage();
-		assertTrue(turbinePage.filterLabel().isDisplayed());
-		assertTrue(turbinePage.user().isDisplayed());
-		log.debug("Login Successfull");
-		turbinePage.user().click();
-		turbinePage.signOut().click();
-	}
 
-	@Test
-	public void invalidUserLogin() throws IOException {
-		fisco = new FileInputStream(path + "\\src\\test\\resources\\properties\\Config.properties");
-		config.load(fisco);
-		LoginPage loginPage = new LoginPage();
-		loginPage.loginApp(config.getProperty("invalidUsername"), config.getProperty("validPassword"));
-		assertTrue(loginPage.incorrectandpasswordlabel().isDisplayed());
-		log.debug("Login Failed");
-	}
-
-	@Test
-	public void invalidPasswordLogin() throws IOException {
-		fisco = new FileInputStream(path + "\\src\\test\\resources\\properties\\Config.properties");
-		config.load(fisco);
-		LoginPage loginPage = new LoginPage();
-		loginPage.loginApp(config.getProperty("validUsername"), config.getProperty("invalidPassword"));
-		assertTrue(loginPage.incorrectandpasswordlabel().isDisplayed());
-		log.debug("Login Failed");
-	}
-
-	@Test
-	public void invalidUsernameInvalidPasswordLogin() throws IOException {
-		fisco = new FileInputStream(path + "\\src\\test\\resources\\properties\\Config.properties");
-		config.load(fisco);
-		LoginPage loginPage = new LoginPage();
-		loginPage.loginApp(config.getProperty("invalidUsername"), config.getProperty("invalidPassword"));
-		assertTrue(loginPage.incorrectandpasswordlabel().isDisplayed());
-		log.debug("Login Failed");
-	}
-	
-	@Test
-	public void verifyLoginPageComponent() throws IOException {
-		LoginPage loginPage = new LoginPage();
-		assertTrue(loginPage.usernameTextBox().isDisplayed());
-		assertTrue(loginPage.passwordTextBox().isDisplayed());
-		assertTrue(loginPage.signInButton().isDisplayed());
-		assertTrue(loginPage.forgotYourPasswordLink().isDisplayed());
-		assertTrue(loginPage.corporateLoginSKFButton().isDisplayed());
-		String label=loginPage.signwithUsernamePasswordLabel().getText();
-		assertTrue(label.equalsIgnoreCase("Sign in with your username and password"));
-		log.debug("Login component verified");
-	}
-	@Test
-	public void forgotYourPasswordVerify() throws IOException {
-		fisco = new FileInputStream(path + "\\src\\test\\resources\\properties\\Config.properties");
-		config.load(fisco);
-		LoginPage loginPage = new LoginPage();
-		loginPage.forgotYourPasswordLink().click();
-		assertTrue(loginPage.forgotPasswordLabel().isDisplayed());
-		assertTrue(loginPage.forgotPasswordMessage().isDisplayed());
-		assertTrue(loginPage.userNameTextBox().isEnabled());
-		assertTrue(loginPage.resetPasswordButton().isDisplayed());
-	}
-	
-	@Test
-	public void checkingPlaceHolder() throws IOException {
-		fisco = new FileInputStream(path + "\\src\\test\\resources\\properties\\Config.properties");
-		config.load(fisco);
-		LoginPage loginPage = new LoginPage();
-		assertEquals(ExpectedUserNamePlaceHolder, loginPage.userNamePlaceHolder());
-		assertEquals(ExpectedPassWordPlaceHolder, loginPage.passWordPlaceHolder());
-		log.debug("PlaceHolder Passed");
 	}
 	
 	@AfterMethod
