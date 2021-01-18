73ebdae4063c89618d631c2da470f09bd286817e
vivek Bhadani <vivek.bhadani167@gmail.com>
2021-01-18 11:25:15 +0530

    supplychain Update

--- src/test/java/com/skf/testcases/SupplyChain.java
+++ src/test/java/com/skf/testcases/SupplyChain.java
@@ -9,6 +9,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import com.skf.base.Pages;
@@ -65,4 +66,10 @@ public class SupplyChain extends Pages{
 		assertTrue(supplyChainPage.availabilityStatusButton5().isDisplayed());
 		assertTrue(supplyChainPage.availabilityStatusButton6().isDisplayed());
     }
+	    @AfterMethod
+		public void tearDown() {
+			Pages.driver.close();
+			driver=null;
+			log.debug("Browser closed");
+		}
 }
