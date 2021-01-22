2a626e9a7995cd2e81631cabb9a570d455774916
vivek Bhadani <vivek.bhadani167@gmail.com>
2021-01-23 00:23:03 +0530

    RoleBased Tc

---
+++ src/test/java/com/skf/testcases/Test_rail_19.java
@@ -0,0 +1,38 @@
+package com.skf.testcases;
+
+import static org.testng.Assert.assertEquals;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import com.skf.base.Pages;
+import com.skf.pages.LandingPage;
+import com.skf.pages.LoginPage;
+
+public class Test_rail_19 extends Pages{
+	String path = System.getProperty("user.dir");
+	public static Properties config = new Properties();
+	public static FileInputStream fisco;
+  @Test
+  public void verifyRail19() throws Exception {
+	  fisco = new FileInputStream(path + "\\src\\test\\resources\\properties\\Config.properties");
+	    config.load(fisco);
+	    LoginPage loginPage = new LoginPage();
+	    loginPage.loginApp(config.getProperty("Test_rail_19_user"),
+	    config.getProperty("Test_rail_19_pass"));
+	    LandingPage landingPage = new LandingPage();
+	    Thread.sleep(15000);
+	    landingPage.supplyChainTilesLabel().isDisplayed();
+	    landingPage.supplyChainLabel().click();
+	    assertEquals(landingPage.supplyChainLabel().getText(),"Supply Chain");
+  }
+  @AfterMethod
+  public void tearDown() {
+	Pages.driver.close();
+	driver=null;
+	log.debug("Browser closed");
+}
+}
---
+++ src/test/java/com/skf/testcases/Test_rail_20.java
@@ -0,0 +1,48 @@
+package com.skf.testcases;
+
+import static org.testng.Assert.assertEquals;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import com.skf.base.Pages;
+import com.skf.pages.AnalyticsPage;
+import com.skf.pages.LandingPage;
+import com.skf.pages.LoginPage;
+
+public class Test_rail_20 extends Pages{
+	String path = System.getProperty("user.dir");
+	public static Properties config = new Properties();
+	public static FileInputStream fisco;
+  @Test
+  public void verifyRail20() throws Exception {
+	  fisco = new FileInputStream(path + "\\src\\test\\resources\\properties\\Config.properties");
+	    config.load(fisco);
+	    LoginPage loginPage = new LoginPage();
+	    loginPage.loginApp(config.getProperty("Test_rail_20_user"),
+	    config.getProperty("Test_rail_20_pass"));
+	    LandingPage landingPage = new LandingPage();
+	    AnalyticsPage analyticspage = new AnalyticsPage();
+	    Thread.sleep(15000);
+	    landingPage.dashboardTilesLabel().isDisplayed();
+	    landingPage.salesTilesLable().isDisplayed();
+	    landingPage.coMoTilesLabel().isDisplayed();
+	    landingPage.dashboardLabel().click();
+	    Thread.sleep(5000);
+	    landingPage.projectDropDown().isDisplayed();
+	    landingPage.analyticsLabel().click();
+	    assertEquals(analyticspage.analyticsSalesLabel().getText(),"Sales");
+	    assertEquals(analyticspage.analyticsCoMoLabel().getText(),"CoMo");
+	    
+	   
+  }
+  @AfterMethod
+  public void tearDown() {
+	Pages.driver.close();
+	driver=null;
+	log.debug("Browser closed");
+}
+}
